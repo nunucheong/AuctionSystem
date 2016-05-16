10ab370ca4d95a3914bcddbb88863b4b8da2bb08
KhooBaoXuan <ramousstyle@gmail.com>
2016-05-16 21:34:46 +0800

    methods in AucSys

--- AuctionSystem/src/auctionsystem/AuctionSystem.java
+++ AuctionSystem/src/auctionsystem/AuctionSystem.java
@@ -1,21 +1,61 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package auctionsystem;
+import java.io.*;
+import java.util.Scanner;
+import java.util.Date;
+import java.text.*;
+import java.util.ArrayList;
 
-/**
- *
- * @author User
- */
 public class AuctionSystem {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        AuctionSystem.checkAvailableAuction();
+    }
+    public static void checkAvailableAuction(){
+        Date current=new Date();
+        SimpleDateFormat simpleFormat =new SimpleDateFormat("dd/MM/yyyy");
+        System.out.println("Today's date : " + simpleFormat.format(current));
+        ArrayList availAuc=new ArrayList();
+        try{
+            String [] split=new String[30];
+            Scanner input=new Scanner(new FileInputStream("d:/AuctionSystem.txt"));
+            while(input.hasNextLine()){
+                String read=input.nextLine();
+                split=read.split(",");
+                
+                Date endTime=simpleFormat.parse(split[2]); //convert String into Date.
+                
+                if(current.before(endTime))
+                    availAuc.add(split[0]); //i assume item name is in first place
+            }
+            System.out.println("Available Auction(s): "+availAuc.toString());
+            input.close();
+        }
+        catch(FileNotFoundException e){
+            e.getMessage();
+        }
+        catch(ParseException e){
+            e.printStackTrace();
+        }
     }
     
+    public void setBidderCall(Date biddingTime, Double biddingAmount, String itemID,Bidder bidder){
+        BiddingStack<Double,Bidder> bid=new BiddingStack<>(); //??
+        bid.push(biddingAmount, bidder);
+    }
+    
+    public void displayCallingPrice(){
+        BiddingStack<Double,Bidder> bid=new BiddingStack<>();
+        System.out.println(bid.toString());
+        //OR  
+        try{
+            Scanner input=new Scanner(new FileInputStream("d:/AuctionSystem.txt"));
+            while(input.hasNextLine()){
+                String read=input.nextLine();
+            }
+           /*....................*/
+        } 
+        catch(FileNotFoundException e){
+            e.getMessage();
+        }
+    }
 }
