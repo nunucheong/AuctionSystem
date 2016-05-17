15373388107eb5fc91f0263886839bb381deb80e
zhongqi22 <kate.wang22@gmail.com>
2016-05-18 00:06:30 +0800

    Update Zhong (seller mode)

--- AuctionSystem/src/auctionsystem/AuctionSystem.java
+++ AuctionSystem/src/auctionsystem/AuctionSystem.java
@@ -53,7 +53,11 @@ public class AuctionSystem {
                         while (selectMode){
                             String choice2 = system.selectModeMenu();
                             switch(choice2){
-                                //Enter seller mode
+                                /*Enter seller mode
+                                 1. Create New Item for sale
+                                 2. Check All Auction
+                                 3. Check Ended Auction
+                                 4. Check Ongoing Auction*/
                                 case "1":                                
                                 //Enter bidder mode
                                 case "2":
@@ -125,7 +129,7 @@ public class AuctionSystem {
     
     //read user profile from userdatabase.txt and create new object<User>
     public String[] readUserProfile(String userId){
-        String[] userData = new String[10]; 
+        String[] userData = new String[11]; 
         try{
             Scanner inputstream = new Scanner (new FileInputStream("database/userdatabase.txt"));
             int i = 0;
@@ -161,7 +165,7 @@ public class AuctionSystem {
         for(String hold : userData[9].split(":")){
             bidderSuccessList.add(itemList.getItem(itemList.indexOfItem(hold)).getValue().getName());
         }
-        bidder = new Bidder (user.getName(), user.getIC(), user.getPaymentType(), user.getAddress(), user.getPhone(),bidderBiddingList,bidderSuccessList); 
+        bidder = new Bidder (user.getName(), user.getIC(), user.getPaymentType(), user.getAddress(), user.getPhone(),Integer.parseInt(userData[10]),bidderBiddingList,bidderSuccessList); 
         return userData;
     }
     
@@ -335,7 +339,7 @@ public class AuctionSystem {
             System.out.println("Error writing to temporary file.");
         }
         
-        String[] userData = new String[10];
+        String[] userData = new String[11];
         int count = 0;
             for(String hold : deleted.split(",")){
                 userData[count] = hold;
@@ -387,6 +391,25 @@ public class AuctionSystem {
         }catch (IOException e){
             System.out.println("Error writing to temporary file.");
         }
+    }
+    
+    public void createNewItem(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\n====== Create New Item ======");
+        System.out.print("Please fill in the item details.\n\nName: ");
+        String itemName = sc.nextLine();
+        System.out.print("\nDescription [not more than 30 characters]: ");
+        String itemDescription = sc.nextLine();
+        System.out.print("\nAuction type: \n1. English Auction\n2. Blind Auction\n3. Vickery Auction\n4. Reserve Auction\nChoose: ");
+        String choice = sc.nextLine();
+        System.out.print("\nAuction start time [dd-mm-yyyy hh:mm:ss]: ");
+        String startTime = sc.nextLine();
+        System.out.print("\nAuction end time [dd-mm-yyyy hh:mm:ss]");
+        String endTime = sc.nextLine();
+        
+        
+        
+        
     }
     
     public static void checkAvailableAuction(){
